7d655f8a711b87919f4d6986525792d0582970d9
kameshwar sah <javacore54@gmail.com>
2022-02-11 12:45:23 +0000

    Updated

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'dart:async';
+
 import 'package:mvvm_image/view_model/list_picture_viewmodel.dart';
-import 'dart:html';
+import 'package:transparent_image/transparent_image.dart';
+
 class HomePgae extends StatefulWidget {
   const HomePgae({Key? key}) : super(key: key);
 
@@ -15,21 +17,36 @@ class _HomePgaeState extends State<HomePgae> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My Movies'),),
+      appBar: AppBar(
+        title: Text('My Movies'),
+      ),
       body: FutureBuilder(
-          future: listPictureViewModel.fetchPictures(),
-          builder: (context, snapshot){
-            if(snapshot.connectionState==ConnectionState.waiting){
-              return Center(child: CircularProgressIndicator(),);
-            }
-            else{
-                return StaggeredGrid.count(
-                    crossAxisCount: 4,
-
-                );
-            }
+        future: listPictureViewModel.fetchPictures(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return StaggeredGridView.countBuilder(
+              crossAxisCount: 2,
+              iteamBuilder: (BuildContext context, int index) => Container(
+                color: Colors.green,
+                child: FadeInImage.memoryNetwork(
+                  placeholder: kTransparentImage,
+                  image:
+                      '${listPictureViewModel.picture![index]!.picSumModel!.downloadUrl}',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              staggeredTileBuilder: (inedx) =>
+                  StaggeredTile.count(2, inedx.isEven ? 2 : 1),
+              mainAxisSpacing: 4.0,
+              crossAxisSpacing: 4.0,
+            );
           }
-      )
+        },
+      ),
     );
   }
 }
